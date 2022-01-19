6243bd33bc5982470ef400c1f64967a1f3b65f69
Yash <yash.v.gutgutia@gmail.com>
2022-01-19 21:44:59 +0530

    Chapter 3 added

---
+++ Ch3/Gridworld.py
@@ -0,0 +1,71 @@
+# Converges approximately to the book example
+
+import numpy as np
+import random
+
+# 0 - L, 1 - U, 2 - R, 3 - D
+class Agent:
+    def __init__(self, maxx, maxy, iters, episodes, discount):
+        self.maxx = maxx
+        self.maxy = maxy
+        self.currx = 0
+        self.curry = 0
+        self.discount = discount
+        self.iters = iters
+        self.episodes = episodes
+        self.state_value = np.zeros(shape=(maxx, maxy))
+        self.state_visit = np.zeros(shape=(maxx, maxy))
+
+    def reset(self):
+        self.currx = np.random.choice(self.maxx)
+        self.curry = np.random.choice(self.maxy)
+
+    def run(self):
+        for i in range(self.iters):
+            for j in range(self.episodes):
+                self.take_step()
+            print("State Value after " + str(i+1) + " iterations: ")
+            print(self.state_value)
+            self.reset()
+
+    def take_step(self):
+        # all random policy    
+        action = np.random.choice(4)
+        self.move(action)
+        
+    def move(self, action):
+        next_x = self.currx
+        next_y = self.curry
+        if(action == 0):
+            next_x += -1
+        elif(action == 1):
+            next_y += -1
+        elif(action == 2):
+            next_x += 1
+        elif(action == 3):
+            next_y += 1
+
+        if(next_x < 0 or next_x >= self.maxx or next_y < 0 or next_y >= self.maxy):
+            self.update_state(self.currx, self.curry, -1)
+        else:
+            self.update_state(next_x, next_y, 0)
+        
+        if(next_x==0 and next_y==1):
+            self.update_state(4, 1, 10)        
+        if(next_x==0 and next_y==3):
+            self.update_state(2, 3, 5)
+    
+    def update_state(self, next_x, next_y, reward):
+        self.state_visit[self.currx][self.curry] += 1
+        self.state_value[self.currx][self.curry] += (self.state_value[next_x][next_y] * self.discount + reward - 
+                        self.state_value[self.currx][self.curry]) / self.state_visit[self.currx][self.curry]
+        self.currx = next_x
+        self.curry = next_y
+
+def main():
+    agent_obj = Agent(5, 5, 100, 1000, 0.9)
+    agent_obj.run()
+    print(agent_obj.state_value)
+        
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Some Codes to work alongside Reinforcement Learning - An Introduction by Richard Sutton and Andrew G. Barto[Link](https://www.amazon.co.jp/exec/obidos/ASIN/0262039249/hatena-blog-22/)
+## Some Codes to work alongside [Reinforcement Learning](https://www.amazon.co.jp/exec/obidos/ASIN/0262039249/hatena-blog-22/) - An Introduction by Richard Sutton and Andrew G. Barto
 
 ### Motivation
 Detailed Solutions for each chapter an be found at [Link](https://github.com/LyWangPX/Reinforcement-Learning-2nd-Edition-by-Sutton-Exercise-Solutions) - an excellent solution guide to help self-learners.
@@ -10,3 +10,6 @@ Some codes are written by me, others might be obtained from the internet, the so
 This is a personal proect done while self-learning. Although the working state of each code is verified by me before updating the repository, some of these might have theoretical/conceptual/semantic errors, which I would be glad to correct when pointed out in issues.
 
 Also incase someone wants to contribute additional code(or better code) are welcome to do so by raising a PR.
+
+## Usage
+Other than OpenAI and Keras, other files should work on all versions of python. But Keras and OpenAI(owing to the use of tensorflow) require <=Python 3.8
